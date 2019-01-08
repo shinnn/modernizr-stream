d24472e0a9acb91d22dbf44d27ef4c88ad250d8f
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2019-01-08 16:22:27 +0900

    cache build results with the same config

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const from2 = require('from2');
 const modernizr = require('modernizr');
 const SafeBuffer = require('safe-buffer').Buffer;
 
+const inspectWithKind = require('inspect-with-kind');
+const isPlainObject = require('lodash/isPlainObject');
+const jsonStableStringifyWithoutJsonify = require('json-stable-stringify-without-jsonify');
 const zeroLengthBuffer = SafeBuffer.alloc(0);
+const caches = new Map();
 
-const ctor = function modernizrStreamCtor(options) {
+const ctor = function modernizrStreamCtor(options = {}) {
 	const ctor = from2.ctor({encoding: 'utf8', ...options}, function readModernizrCode(size, next) {
 		if (ctor.modernizrCode === null) {
 			setImmediate(() => next(null, zeroLengthBuffer));
 			return;
+		}
+
+		if (!isPlainObject(options)) {
+			const error = new TypeError(`Expected an <Object> to set modernizr-stream options, but got ${
+				inspectWithKind(options)
+			}.`);
+
+			error.code = 'ERR_INVALID_ARG_TYPE';
+			throw error;
 		}
 
 		if (!this.firstModernizrChunkEmitted) {
@@ -34,9 +47,17 @@ const ctor = function modernizrStreamCtor(options) {
 	ctor.prototype.unreadModernizrCode = null;
 	ctor.prototype.firstModernizrChunkEmitted = false;
 
-	modernizr.build(options, result => {
-		ctor.modernizrCode = result;
-	});
+	const cacheKey = jsonStableStringifyWithoutJsonify(options);
+	const cache = caches.get(cacheKey);
+
+	if (cache !== undefined) {
+		ctor.modernizrCode = cache;
+	} else {
+		modernizr.build(options, result => {
+			ctor.modernizrCode = result;
+			caches.set(cacheKey, result);
+		});
+	}
 
 	return ctor;
 };
